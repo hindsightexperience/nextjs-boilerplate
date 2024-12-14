0d3c93514c52f4bb368d39d82518f65de8b7229f
jennifer santiago <jennifersantiago@jennifesMacBook.attlocal.net>
2024-12-13 20:33:06 -0600

    Fix ESLint errors and update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,24 +5,15 @@ import React, { useState } from "react";
 import VaultAnimation from "../components/VaultAnimation";
 import { ReactNode } from "react";
 
-import { Roboto } from "next/font/google";
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const roboto = Roboto({
-  variable: "--font-roboto",
-  subsets: ["latin"],
-  weight: ["400", "700"],
-});
-
 interface RootLayoutProps {
   children: ReactNode;
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
   const [animationComplete, setAnimationComplete] = useState(false);
-  
+
   return (
-    <html lang="en" className={`${roboto.variable} ${roboto.variable}`}>
+    <html lang="en">
       <body className="bg-black text-white">
         {!animationComplete ? (
           <VaultAnimation onComplete={() => setAnimationComplete(true)} />
@@ -32,5 +23,4 @@ export default function RootLayout({ children }: RootLayoutProps) {
       </body>
     </html>
   );
-  
 }
