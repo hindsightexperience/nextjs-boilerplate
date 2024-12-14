ccecd3faadc09b7bf9f912149fbf61803b3cca96
jennifer santiago <jennifersantiago@jennifesMacBook.attlocal.net>
2024-12-13 20:20:50 -0600

    Fix ESLint errors and update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,17 +5,11 @@ import React, { useState } from "react";
 import VaultAnimation from "../components/VaultAnimation";
 import { ReactNode } from "react";
 
-import { Roboto, Roboto_Mono } from "next/font/google";
+import { Roboto } from "next/font/google";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const roboto = Roboto({
   variable: "--font-roboto",
-  subsets: ["latin"],
-  weight: ["400", "700"],
-});
-
-const robotoMono = Roboto_Mono({
-  variable: "--font-roboto-mono",
   subsets: ["latin"],
   weight: ["400", "700"],
 });
@@ -26,12 +20,10 @@ interface RootLayoutProps {
 
 export default function RootLayout({ children }: RootLayoutProps) {
   const [animationComplete, setAnimationComplete] = useState(false);
-
+  
   return (
-    <html lang="en">
-      <body
-        className={`${roboto.variable} ${robotoMono.variable} bg-black text-white`}
-      >
+    <html lang="en" className={`${roboto.variable} ${roboto.variable}`}>
+      <body className="bg-black text-white">
         {!animationComplete ? (
           <VaultAnimation onComplete={() => setAnimationComplete(true)} />
         ) : (
@@ -40,4 +32,5 @@ export default function RootLayout({ children }: RootLayoutProps) {
       </body>
     </html>
   );
+  
 }
