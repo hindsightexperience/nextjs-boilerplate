d63c625a645b9cbdbd539a73b2d6aa82be8a5123
jennifer santiago <jennifersantiago@jennifesMacBook.attlocal.net>
2024-12-15 13:56:51 -0600

    Fix Not Found Page Errors add

--- app/_not-found/page.tsx
+++ app/_not-found/page.tsx
@@ -1,14 +1,11 @@
 "use client";
-
-import dynamic from "next/dynamic";
-
 export default function BrowserOnlyNotFoundPage() {
-    return (
-      <div>
-        <h1>404 - Page Not Found</h1>
-        <p>This page does not exist.</p>
-      </div>
-    );
-  }
-  
-  console.log("Is this running in the browser?", typeof document !== "undefined");
+  return (
+    <div>
+      <h1>404 - Page Not Found</h1>
+      <p>This page does not exist.</p>
+    </div>
+  );
+}
+
+console.log("Is this running in the browser?", typeof document !== "undefined");
